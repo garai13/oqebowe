b15cc3558ba5db8803717a6ebc64e6c6de87e124
Школа <garai13aizat143@mail.ru>
2019-11-29 17:14:52 +0300

    0.1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String action = scan.next();
+    int n = scan.nextInt();
+
+    Stack stack = new Stack();
+    stack.push(n);
+
+
+    }
+}
